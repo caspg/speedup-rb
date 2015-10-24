8b8bfcbe45c51139239e9783b4320f9d79c789c3
Kacper Goliński <kacper.golinski@netguru.co>
2015-10-24 15:27:41 +0200

    call RepoAnalyzer Service from report worker

---
+++ spec/web/workers/report_spec.rb
@@ -0,0 +1,13 @@
+describe Web::Workers::Report do
+  let(:worker) { described_class.new }
+  let(:report_id) { 007 }
+  let(:repo_analyzer) { double }
+
+  it 'calls RepoAnalyzer service with proper attribute' do
+    expect(Web::Services::RepoAnalyzer).to receive(:new).with(report_id)
+      .and_return(repo_analyzer)
+    expect(repo_analyzer).to receive(:call)
+
+    worker.perform(report_id)
+  end
+end
