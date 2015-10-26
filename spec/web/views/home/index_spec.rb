f44ec8c02f5380129e627e8906b3875c11154032
Kacper Goliński <kacper.golinski@netguru.co>
2015-10-26 21:16:03 +0100

    fix spec for real

--- spec/web/views/home/index_spec.rb
+++ spec/web/views/home/index_spec.rb
@@ -11,18 +11,15 @@ end
 describe describe Web::Views::Home::Index do
   let(:params)    { NewReportParams.new({}) }
   let(:exposures) { Hash[params: params] }
-  let(:view)      { Web::Views::Home::Index.new(template, exposures) }
+  let(:template)  { Lotus::View::Template.new('apps/web/templates/home/index.html.erb') }
+  let(:view)      { Web::Views::Home::Index.new(template, exposures.merge(format: :html)) }
   let(:rendered)  { view.render }
 
-  describe '_form partial' do
-    let(:template)  { Lotus::View::Template.new('apps/web/templates/home/_form.html.erb') }
+  it 'displays list of errors when params contains errors' do
+    params.valid?
 
-    it 'displays list of errors when params contains errors' do
-      params.valid?
-
-      expect(rendered).to include('There was a problem with your submission')
-      expect(rendered).to include('owner is required')
-      expect(rendered).to include('repo is required')
-    end
+    expect(rendered).to include('There was a problem with your submission')
+    expect(rendered).to include('owner is required')
+    expect(rendered).to include('repo is required')
   end
 end
