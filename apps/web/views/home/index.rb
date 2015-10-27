d011e421a382acc590a4c39d7cfc8af91985fe5e
Kacper Goliński <kacper.golinski@netguru.co>
2015-10-27 19:49:15 +0100

    include EasyErrors in index and create view

--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -1,5 +1,6 @@
 module Web::Views::Home
   class Index
     include Web::View
+    include Web::Views::EasyErrors
   end
 end
--- apps/web/views/reports/create.rb
+++ apps/web/views/reports/create.rb
@@ -1,6 +1,7 @@
 module Web::Views::Reports
   class Create
     include Web::View
+    include Web::Views::EasyErrors
     template 'home/index'
   end
 end
