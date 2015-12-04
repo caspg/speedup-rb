05a22af07f02e81995a782bc2e60f788254f499e
Kacper Golinski <kacper.golinski@netguru.co>
2015-12-04 20:43:52 +0100

    update worker - no retry

--- apps/web/workers/report.rb
+++ apps/web/workers/report.rb
@@ -1,6 +1,7 @@
 module Web::Workers
   class Report
     include Sidekiq::Worker
+    sidekiq_options :retry => false
 
     def perform(report_id)
       Web::Services::ReportWorkerFlow.new(report_id).call
